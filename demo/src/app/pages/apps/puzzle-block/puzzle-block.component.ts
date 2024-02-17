fcb2ab2807d27002b38b7f421e5028f4dc1df416
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-17 15:07:04 +0000

    update

--- demo/src/app/pages/apps/puzzle-block/puzzle-block.component.ts
+++ demo/src/app/pages/apps/puzzle-block/puzzle-block.component.ts
@@ -1,11 +1,18 @@
-import { NgClass, NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { fadeInUp400ms } from '@vex/animations/fade-in-up.animation';
 import { stagger40ms } from '@vex/animations/stagger.animation';
+
+
+import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { VexScrollbarComponent } from '@vex/components/vex-scrollbar/vex-scrollbar.component';
 
 import { CdkDrag } from '@angular/cdk/drag-drop';
 
@@ -27,7 +34,22 @@ import { CdkDrag } from '@angular/cdk/drag-drop';
     RouterLinkActive,
     NgClass,
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    NgClass,
+    NgIf,
+    VexScrollbarComponent,
+    NgFor,
+    MatRippleModule,
+    RouterLinkActive,
+    RouterLink,
+    RouterOutlet,
+    AsyncPipe,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule
   ],
 })
 
