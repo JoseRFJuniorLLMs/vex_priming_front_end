fd6924f31af4d048dbb477711dd2102a5b8f56cb
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-17 09:56:26 +0000

    drag drop list

---
+++ demo/src/app/pages/apps/game/puzzle_block/cdk-drag-drop-axis-lock-example.ts
@@ -0,0 +1,14 @@
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+
+/**
+ * @title Drag&Drop position locking
+ */
+@Component({
+  selector: 'cdk-drag-drop-axis-lock-example',
+  templateUrl: 'cdk-drag-drop-axis-lock-example.html',
+  styleUrls: ['cdk-drag-drop-axis-lock-example.scss'],
+  standalone: true,
+  imports: [CdkDrag],
+})
+export class CdkDragDropAxisLockExample {}
