6c47a6bb2ce48463b63b0875f03dc647e77719e7
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-21 19:05:40 +0000

    update toggle

--- demo/src/app/layouts/components/footer/footer.component.ts
+++ demo/src/app/layouts/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ShareBottomSheetComponent } from './../../../pages/dashboards/components/share-bottom-sheet/share-bottom-sheet.component';
 
 import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -8,14 +9,22 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { scaleInOutAnimation } from '@vex/animations/scale-in-out.animation';
 
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ShareBottomWimHofComponent } from '../../../../../src/app/pages/dashboards/components/share-bottom-wim-hof/share-bottom-wim-hof.component';
 
 @Component({
   selector: 'vex-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [scaleInOutAnimation],
   standalone: true,
   imports: [
     MatButtonModule,
@@ -28,12 +37,16 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatSlideToggleModule,
     MatBadgeModule,
     MatProgressSpinnerModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    ShareBottomWimHofComponent,
+    ShareBottomSheetComponent,
+    MatBottomSheetModule,
+
   ]
 })
 export class FooterComponent implements OnInit, OnDestroy {
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef, private _bottomSheet: MatBottomSheet, private dialog: MatDialog) { }
 
   displayTime: string = '30:00';
   timer: any;
@@ -49,9 +62,19 @@ export class FooterComponent implements OnInit, OnDestroy {
   paused: boolean = false;
   remainingSeconds: number = 0;
 
+  progress: number = 0;
+
 changeIcons(): void {
   this.icon60fps = 'mat:60fps_select';
   this.icon30fps = 'mat:30fps_select';
+}
+
+ngOnInit(): void {
+  this.startTimer();
+}
+
+ngOnDestroy(): void {
+  this.stopTimer();
 }
 
 onToggleChange(selection: string) {
@@ -79,16 +102,6 @@ onToggleChange(selection: string) {
   }
 }
 
-
-  ngOnInit(): void {
-    this.startTimer();
-  }
-
-  ngOnDestroy(): void {
-    this.stopTimer();
-  }
-
-
   startTimer(): void {
     if (this.paused) {
       // Se estiver pausado, continue de onde parou
@@ -101,6 +114,7 @@ onToggleChange(selection: string) {
         if (this.remainingSeconds === 0) {
           this.stopTimer();
           //this.openDialog();
+          this.openBothConfigs();
         } else {
           this.remainingSeconds--;
         }
@@ -114,7 +128,7 @@ onToggleChange(selection: string) {
         const minutes = Math.floor(this.remainingSeconds / 60);
         const seconds = this.remainingSeconds % 60;
         this.displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-        this.cdr.detectChanges();  // Adicione esta linha
+        this.cdr.detectChanges();
         this.remainingSeconds--;
       }, 1000);
     }
@@ -131,13 +145,9 @@ pauseTimer(): void {
 
 continueTimer(): void {
   if (!this.paused) return;
-
-  // Reinicie o timer com o tempo restante
   this.startTimer();
   this.paused = false;
 }
-
-progress: number = 0;
 
 stopProgressBar(): void {
   clearInterval(this.timer);
@@ -145,5 +155,8 @@ stopProgressBar(): void {
   this.progress = 0;
 }
 
+openBothConfigs() {
+  this._bottomSheet.open(ShareBottomWimHofComponent);
+}
 
 }
