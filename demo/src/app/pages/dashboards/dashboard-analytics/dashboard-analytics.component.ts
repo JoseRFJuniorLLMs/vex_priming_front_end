9c30d943ff8f8a557b8475fff29aa7d4a0b56672
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-03-03 06:32:44 +0000

    update

--- demo/src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.component.ts
+++ demo/src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.component.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { VexBreadcrumbsComponent } from '@vex/components/vex-breadcrumbs/vex-breadcrumbs.component';
 import { VexSecondaryToolbarComponent } from '@vex/components/vex-secondary-toolbar/vex-secondary-toolbar.component';
+import gpt4 from 'gpt4.json';
 import { WidgetAssistantComponent } from '../components/widgets/widget-assistant/widget-assistant.component';
 import { WidgetLargeChartComponent } from '../components/widgets/widget-large-chart/widget-large-chart.component';
 import { WidgetLargeGoalChartComponent } from '../components/widgets/widget-large-goal-chart/widget-large-goal-chart.component';
@@ -20,12 +21,15 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 
-const gptUrl = "https://api.openai.com/v1/chat/completions";
-const gptApiKey = "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";
-
 import screenfull from 'screenfull';
+
+// Interface para descrever a estrutura da resposta da API
+interface ResponseData {
+  choices?: { message: { content: string } }[];
+}
 
 @Component({
   selector: 'vex-dashboard-analytics',
@@ -47,7 +51,8 @@ import screenfull from 'screenfull';
     MatTabsModule,
     VexPageLayoutContentDirective,
     VexPageLayoutHeaderDirective,
-    VexPageLayoutComponent
+    VexPageLayoutComponent,
+    MatCardModule
   ]
 })
 export class DashboardAnalyticsComponent implements OnInit {
@@ -57,6 +62,8 @@ export class DashboardAnalyticsComponent implements OnInit {
   chatMessage: any;
   isLoading = false;
   errorText = '';
+  data: any;
+
 
   constructor(
     private http: HttpClient
@@ -72,25 +79,32 @@ export class DashboardAnalyticsComponent implements OnInit {
   this.isLoading = true;
   try {
     const headers = new HttpHeaders({
-      "Authorization": `Bearer ${gptApiKey}`,
+      "Authorization": `Bearer ${gpt4.gptApiKey}`,
       "Content-Type": "application/json"
     });
 
-    const response = await this.http.post(gptUrl, {
+    const response: ResponseData | undefined = await this.http.post<ResponseData>(gpt4.gptUrl, {
       messages: [{ role: 'user', content: question }],
       temperature: 0.5,
       max_tokens: 4000,
       model: "gpt-4",
     }, { headers }).toPromise();
 
-    this.chatMessage = response;
+    // Verificando se a resposta é indefinida
+    if (response === undefined) {
+      throw new Error("Resposta indefinida.");
+    }
 
+    // Verificando se a propriedade 'choices' está presente na resposta
+    if (response.choices && response.choices.length > 0) {
+      this.chatMessage = response.choices[0].message.content;
+    } else {
+      throw new Error("Resposta inválida.");
+    }
   } catch (error) {
     this.errorText = (error as any).error.message;
   } finally {
     this.isLoading = false;
   }
 }
-
-
 }
