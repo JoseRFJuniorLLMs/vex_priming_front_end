57d013ccc1b750ba6154c3247afd120c7f8ccce5
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-03-10 21:01:14 +0000

    import ePub from 'epubjs';

--- demo/src/app/pages/apps/book/book.component.ts
+++ demo/src/app/pages/apps/book/book.component.ts
@@ -4,7 +4,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select'; // Importar MatSelectModule
+import { MatSelectModule } from '@angular/material/select';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition
+} from '@angular/material/snack-bar';
 
 import ePub from 'epubjs';
 import gpt4 from '../../../../../gpt4.json';
@@ -25,13 +30,18 @@ export class BookComponent implements OnInit {
   selectedText: string = '';
   totalPages: number = 0;
   currentPage: number = 0;
+  durationInSeconds = 130;
 
-  constructor(private http: HttpClient) {}
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar,) {}
 
   ngOnInit() {
     this.initializeBook();
   }
 
+  //initializeBook
   async initializeBook() {
     try {
       this.book = ePub("../../assets/epub/TheLittlePrince.epub");
@@ -39,43 +49,18 @@ export class BookComponent implements OnInit {
 
       this.rendition = this.book.renderTo("area-de-exibicao");
 
-      // Função para atualizar as configurações de renderização
-      this.rendition.updateRenderMethod = (options: any) => {
-        // Limpa a área de exibição antes de aplicar novas configurações
-        this.rendition.clear();
-
-        if (options.method) {
-          // Se a opção 'method' for fornecida, ajuste-a conforme necessário
-          // Nota: Esta é uma sugestão conceitual. Adapte conforme as APIs disponíveis do EPUB.js
-          this.rendition.settings.method = options.method;
-        }
-        if (options.flow) {
-          // Se a opção 'flow' for fornecida, ajuste-a conforme necessário
-          this.rendition.flow(options.flow);
-        }
-        if (options.width && options.height) {
-          // Ajusta a largura e a altura da área de renderização
-          this.rendition.resize(options.width, options.height);
-        }
-
-        // Reexibe o conteúdo após atualizar as configurações
-        this.rendition.display();
-      };
+      // Gera as localizações.
+      await this.book.locations.generate();
+      this.totalPages = this.book.locations.length(); // Atualiza o total de páginas
 
       this.rendition.display();
-
-      // Event listener para seleção de texto
-      this.rendition.on('selection', (cfiRange: any, contents: any) => {
-        this.selectedText = contents.text();
-        // Pode-se adicionar lógica adicional aqui, como manipulação de seleção de texto
-      });
-
+      // Outro código...
     } catch (error) {
       console.error("Error loading or rendering book: ", error);
     }
   }
 
-
+  //getCurrentPageText
   async getCurrentPageText(): Promise<string> {
     if (!this.rendition || !this.rendition.location) {
       return '';
@@ -89,28 +74,35 @@ export class BookComponent implements OnInit {
     }
 
     return currentPage.getText();
+
   }
 
+  //toggleLayout
   toggleLayout() {
     return () => {
       this.rendition.spread = (this.rendition.spread === "none") ? "always" : "none";
       this.rendition.display();
+      this.openSnackBar(this.rendition.display());
     };
   }
 
   nextPage() {
+    this.openSnackBar("nextPage"+this.rendition.next().length);
     this.rendition.next();
   }
 
   prevPage() {
+    this.openSnackBar("prevPage"+this.rendition.prev().length);
     this.rendition.prev();
   }
 
   zoomIn() {
+    this.openSnackBar("zoomIn");
     this.rendition.themes.fontSize('120%');
   }
 
   zoomOut() {
+    this.openSnackBar("zoomOut");
     this.rendition.themes.fontSize('100%');
   }
 
@@ -119,9 +111,11 @@ export class BookComponent implements OnInit {
   }
 
   copySelectedText() {
+    this.openSnackBar("copySelectedText");
     navigator.clipboard.writeText(this.selectedText);
   }
 
+  //sendCurrentPageTextToAI
   async sendCurrentPageTextToAI() {
     const currentPageText = await this.getCurrentPageText();
 
@@ -130,6 +124,7 @@ export class BookComponent implements OnInit {
       "Content-Type": "application/json"
     });
 
+    //body
     const body = {
       prompt: currentPageText,
       max_tokens: 10,
@@ -137,20 +132,26 @@ export class BookComponent implements OnInit {
       model: "gpt-4",
     };
 
+    //post
     this.http.post(gpt4.gptUrl, body, { headers }).subscribe((response: any) => {
       console.log("Resposta da OpenAI:", response);
+      this.openSnackBar(response);
     }, (error: any) => {
       console.error("Erro ao enviar texto para a OpenAI: ", error);
     });
-  }
+  }//fim IA
 
+  //countPages
   async countPages(): Promise<number> {
     const numberOfPages = await this.book.locations.length;
+    this.openSnackBar("countPages:"+numberOfPages);
     return numberOfPages;
   }
 
+  //getCurrentPage
   getCurrentPage(): number {
     const currentPageIndex = this.book && this.book.navigation && this.book.navigation.indexOf(this.book.currentLocation);
+    this.openSnackBar("countPages:"+ currentPageIndex + 1);
     return currentPageIndex + 1;
   }
 
@@ -162,34 +163,50 @@ export class BookComponent implements OnInit {
 
     switch(option) {
       case 'default':
+        this.openSnackBar("Default");
         // Possivelmente, manter padrões ou aplicar configurações específicas
         break;
       case 'continuous':
+        this.openSnackBar("Continuous");
         flowValue = 'scrolled-doc'; // ou 'scrolled' dependendo da versão do epub.js
         break;
       case 'paginated':
+        this.openSnackBar("Paginated");
         flowValue = 'paginated';
         width = '900px';
         height = '600px';
         break;
       case 'auto':
+        this.openSnackBar("Auto");
         // Definir lógica para 'auto', se aplicável
         break;
     }
 
     if (flowValue) {
+      this.openSnackBar("flowValue");
       this.rendition.flow(flowValue);
     }
 
     if (width && height) {
+      this.openSnackBar("height");
       this.rendition.resize(width, height);
     }
 
     // Re-renderizar o conteúdo no ponto atual
     const currentLocation = this.rendition.currentLocation();
     if (currentLocation) {
+      this.openSnackBar(currentLocation.stringify(currentLocation(currentLocation)));
       this.rendition.display(currentLocation.start.cfi);
     }
   }
 
-}
+  /* ==================SNACK BAR==================== */
+  openSnackBar(textDisplay: string) {
+    const snackBarRef = this._snackBar.open(textDisplay, "Close", {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 100,
+    });
+  }
+
+}//fim
